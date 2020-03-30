c91889780e8a11bec7b3aaadca202450b0650bc7
Klivenko Maxim <piqcmax@gmail.com>
2020-03-30 10:44:02 +0600

    adding tags

--- src/main/java/me/klivenko/leetcode/solved/P529_Minesweeper.java
+++ src/main/java/me/klivenko/leetcode/solved/P529_Minesweeper.java
@@ -1,12 +1,9 @@
 package me.klivenko.leetcode.solved;
 
-import javafx.util.Pair;
 import me.klivenko.leetcode.common.Assert;
 import me.klivenko.leetcode.common.Utils;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 /*
@@ -70,6 +67,10 @@ The range of the input matrix's height and width is [1,50].
 The click position will only be an unrevealed square ('M' or 'E'), which also means the input board contains at least one clickable square.
 The input board won't be a stage when game is over (some mines have been revealed).
 For simplicity, not mentioned rules should be ignored in this problem. For example, you don't need to reveal all the unrevealed mines when the game is over, consider any cases that you will win the game or flag any squares.
+ */
+
+/*
+    tags: bfs, dfs, graph
  */
 
 /*
