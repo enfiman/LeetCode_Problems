d7a445fca13afa0ab6454b98b5a37b32a5aad994
Klivenko Maxim <piqcmax@gmail.com>
2020-04-04 12:51:49 +0600

    https://leetcode.com/problems/word-search/submissions/

--- src/main/java/me/klivenko/leetcode/top_interview_questions/medium/backtracking/Word_Search.java
+++ src/main/java/me/klivenko/leetcode/top_interview_questions/medium/backtracking/Word_Search.java
@@ -114,8 +114,6 @@ public class Word_Search {
         }
 
         public boolean exist(char[][] board, String word) {
-
-
             for (int i = 0; i < board.length; i++) {
                 for (int j = 0; j < board[0].length; j++) {
                     if (board[i][j] == word.charAt(0)) {
