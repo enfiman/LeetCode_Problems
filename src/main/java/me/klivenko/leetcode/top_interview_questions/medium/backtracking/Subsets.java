af40d02c5b3600e6d8cc8a0f051c5f5cc0669807
Klivenko Maxim <piqcmax@gmail.com>
2020-04-06 09:29:12 +0600

    https://leetcode.com/explore/interview/card/top-interview-questions-medium/109/backtracking/796/ adding description

--- src/main/java/me/klivenko/leetcode/top_interview_questions/medium/backtracking/Subsets.java
+++ src/main/java/me/klivenko/leetcode/top_interview_questions/medium/backtracking/Subsets.java
@@ -4,6 +4,30 @@ import me.klivenko.leetcode.common.Utils;
 
 import java.util.*;
 
+/*
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+ */
+
+/*
+https://leetcode.com/explore/interview/card/top-interview-questions-medium/109/backtracking/796/
+ */
 public class Subsets {
     public static void main(String[] args) {
 
