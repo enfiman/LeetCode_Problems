54aa8284ab777a3523d2c1e6f88b4d3e415486a0
Klivenko Maxim <piqcmax@gmail.com>
2020-04-07 16:06:55 +0600

    https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/528/week-1/3289/

--- src/main/java/me/klivenko/leetcode/challenge/april_2020/Day7.java
+++ src/main/java/me/klivenko/leetcode/challenge/april_2020/Day7.java
@@ -1,21 +1,38 @@
 package me.klivenko.leetcode.challenge.april_2020;
 
 /*
-Given an array of strings, group anagrams together.
+Given an integer array arr, count element x such that x + 1 is also in arr.
 
-Example:
+If there're duplicates in arr, count them seperately.
 
-Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
-Output:
-[
-  ["ate","eat","tea"],
-  ["nat","tan"],
-  ["bat"]
-]
-Note:
 
-All inputs will be in lowercase.
-The order of your output does not matter.
+
+Example 1:
+
+Input: arr = [1,2,3]
+Output: 2
+Explanation: 1 and 2 are counted cause 2 and 3 are in arr.
+Example 2:
+
+Input: arr = [1,1,3,3,5,5,7,7]
+Output: 0
+Explanation: No numbers are counted, cause there's no 2, 4, 6, or 8 in arr.
+Example 3:
+
+Input: arr = [1,3,2,3,5,0]
+Output: 3
+Explanation: 0, 1 and 2 are counted cause 1, 2 and 3 are in arr.
+Example 4:
+
+Input: arr = [1,1,2,2]
+Output: 2
+Explanation: Two 1s are counted cause 2 is in arr.
+
+
+Constraints:
+
+1 <= arr.length <= 1000
+0 <= arr[i] <= 1000
 */
 
 import me.klivenko.leetcode.common.Assert;
@@ -24,7 +41,7 @@ import me.klivenko.leetcode.common.Utils;
 import java.util.*;
 
 /*
-    https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/528/week-1/3288/
+    https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/528/week-1/3289/
  */
 public class Day7 {
     public static void main(String[] args) {
