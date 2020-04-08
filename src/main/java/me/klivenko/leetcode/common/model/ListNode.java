8de63157b596da7bf90c70d946d527584fd76afb
Klivenko Maxim <piqcmax@gmail.com>
2020-04-09 00:03:07 +0600

    https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3290/

---
+++ src/main/java/me/klivenko/leetcode/common/model/ListNode.java
@@ -0,0 +1,18 @@
+package me.klivenko.leetcode.common.model;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
